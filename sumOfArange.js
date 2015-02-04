05f3e442b930f3f778f152a40f66553848c6a116
Phillip <phv9495@saintjoe.edu>
2015-02-03 20:46:14 -0500

    This is the sumOfArange program before the bonus portion of the problem.

---
+++ sumOfArange.js
@@ -0,0 +1,27 @@
+//Phil Hofer
+//2/3/2015
+//sumOfArange.js
+//This program takes the range of two numbers and calculates the sum of
+//this range. 
+
+//This function takes the range of two numbers and puts them into an array.
+function range(start, end) {
+  var nums = [];
+  for (var i=start;i<=end;i++) {
+    nums.push(i);
+  }
+  return nums;
+}
+
+//This function sums up the numbers in an array and retruns the total.
+function sum(arr) {
+  var total = 0;
+  for (var i=0;i<arr.length;i++) {
+    total += arr[i];
+    //console.log("This is total: " + total);
+  }
+  return total;
+}
+
+console.log(range(1,10));
+console.log(sum(range(1, 10)));
